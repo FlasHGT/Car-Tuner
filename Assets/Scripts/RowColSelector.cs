067a61dd0dcd8211d69f69559c29af29b8797ae3
George <thebrainycraft@gmail.com>
2019-10-13 20:48:20 +0300

    0.5.2 - Added Selectable.cs and DragSelection.cs

---
+++ Assets/Scripts/RowColSelector.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RowColSelector : MonoBehaviour
+{
+	public Main main = null;
+
+	[SerializeField] Button button = null;
+	[SerializeField] InputField valueInputField = null;
+
+	[SerializeField] InputField[] inputFields = null; 
+
+    public void ActivateInputField ()
+	{
+		if(button.gameObject.activeInHierarchy && !main.inputFieldBeingEdited)
+		{
+			button.gameObject.SetActive(false);
+			valueInputField.gameObject.SetActive(true);
+			main.inputFieldBeingEdited = true;
+
+			valueInputField.text = 0.ToString();
+			valueInputField.textComponent.text = 0.ToString();
+
+			for (int x = 0; x < inputFields.Length; x++)
+			{
+				inputFields[x].GetComponent<Image>().color = Color.yellow;
+			}
+		}
+	}
+
+	public void ApplyValue ()
+	{
+		foreach(InputField i in inputFields)
+		{
+			float newValue = float.Parse(i.text) + float.Parse(valueInputField.text);
+			i.text = newValue.ToString();
+			i.textComponent.text = newValue.ToString();
+			i.GetComponent<Image>().color = Color.white;
+		}
+
+		valueInputField.text = string.Empty;
+		valueInputField.gameObject.SetActive(false);
+		button.gameObject.SetActive(true);
+		main.inputFieldBeingEdited = false;
+	}
+
+	private void Update ()
+	{
+		if(valueInputField.gameObject.activeInHierarchy && Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			float newFloat = 0f;
+
+			if(valueInputField.text == string.Empty)
+			{
+				newFloat = 0f + 0.1f;
+			}else
+			{
+				newFloat = float.Parse(valueInputField.text) + 0.1f;
+			}
+
+			valueInputField.text = newFloat.ToString();
+			valueInputField.textComponent.text = newFloat.ToString();
+		}
+		else if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			float newFloat = 0f;
+
+			if (valueInputField.text == string.Empty)
+			{
+				newFloat = 0f + 0.1f;
+			}
+			else
+			{
+				newFloat = float.Parse(valueInputField.text) - 0.1f;
+			}
+
+			valueInputField.text = newFloat.ToString();
+			valueInputField.textComponent.text = newFloat.ToString();
+		}
+	}
+}
---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Selectable : MonoBehaviour, ISelectHandler, IPointerClickHandler, IDeselectHandler
+{
+	public Main main = null;
+
+	public static HashSet<Selectable> allMySelectables = new HashSet<Selectable>();
+	public static HashSet<Selectable> currentlySelected = new HashSet<Selectable>();
+
+	private Image image = null;
+
+	private void Awake ()
+	{
+		allMySelectables.Add(this);
+	}
+
+	private void Start ()
+	{
+		image = GetComponent<Image>();
+	}
+
+	public void OnDeselect(BaseEventData eventData)
+	{
+		if(!main.inputFieldBeingEdited)
+		{
+			image.color = Color.white;
+		}
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
+		{
+			DeselectAll(eventData);
+		}
+
+		if(!main.inputFieldBeingEdited)
+		{
+			OnSelect(eventData);
+		}
+	}
+
+	public void OnSelect(BaseEventData eventData)
+	{
+		if(!main.inputFieldBeingEdited)
+		{
+			currentlySelected.Add(this);
+			image.color = Color.yellow;
+		}
+	}
+
+	public static void DeselectAll (BaseEventData eventData)
+	{
+		foreach (Selectable selectable in currentlySelected)
+		{
+			selectable.OnDeselect(eventData);
+		}
+		currentlySelected.Clear();
+	}
+}
